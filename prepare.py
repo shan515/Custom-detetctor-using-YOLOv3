47abe9f609461b340631197e5664bc897a95d9d9
shan515 <shantanupande898@gmail.com>
2020-07-06 20:03:14 +0530

    Script for preparing val and train set

---
+++ prepare.py
@@ -0,0 +1,25 @@
+import glob, os
+
+
+dataset_path = '/home/hp/Desktop/knife_custom/knife'
+colab_path = '/content/darknet/knife'
+
+# Percentage of images to be used for the test set
+percentage_test = 10;
+
+# Create and/or truncate train.txt and test.txt
+file_train = open('train.txt', 'w')  
+file_test = open('val.txt', 'w')
+
+# Populate train.txt and test.txt
+counter = 1  
+index_test = round(100 / percentage_test)  
+for pathAndFilename in glob.iglob(os.path.join(dataset_path, "*.jpg")):  
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+    if counter == index_test+1:
+        counter = 1
+        file_test.write(colab_path + "/" + title + '.jpg' + "\n")
+    else:
+        file_train.write(colab_path + "/" + title + '.jpg' + "\n")
+        counter = counter + 1
